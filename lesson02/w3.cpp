dc3231a4bdcde489d8dc1054db8a951a9d375593
Nick Kovalchuk <nikitakovalchuk05@gmail.com>
2025-01-07 16:05:43 +0200

    lesson2 added

---
+++ lesson02/w3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//програма мала два синтаксичні баги у троці виводу.
+//був пропушені лапки на початку та точка з комою у кінці.
+
+int main()
+{
+	cout << "Is there a bug here?";
+	return 0;
+}
